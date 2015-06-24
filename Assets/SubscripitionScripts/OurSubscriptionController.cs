fa09e6ba95db21119d7b578d789c7dcb92340ba4
Erik Balthazar <erik.balthazar@movile.com>
2015-06-24 12:34:05 -0300

    Create Subscription controller

---
+++ Assets/SubscripitionScripts/OurSubscriptionController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Movile;
+using System;
+
+public class OurSubscriptionController : MonoBehaviour {
+
+	static string trimestral = "com.movile.android.oikids.trimestral";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("TESTE - Subscription inicio");
+		Subscription.InitWithApplicationId("");
+		Debug.Log ("TESTE - Subscription inicializado");
+		Subscription.UpdateAllSubscriptions ();
+		Debug.Log ("TESTE - Subscription atualizado");
+		bool subscribed = Subscription.CheckActiveKiwiSubscription (trimestral);
+		Debug.Log ("TESTE - Subscription fim: "+subscribed);
+	}
+}
