3352c27e8fe4ef441a2293246bfa49b441b26916
Erik Balthazar <erik.balthazar@movile.com>
2015-07-16 15:03:37 -0300

    Made UnityPlayerAcitivity be successfully recognized by the engine.

--- Assets/Scripts/ClickPluginAndroid.cs
+++ Assets/Scripts/ClickPluginAndroid.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ClickPluginAndroid : MonoBehaviour {
 
-	public void ClickUsePlugin() {
+	public Text textField;
 
+	public void ClickUsePlugin() {
+		/*
 		AndroidJavaClass unity = new AndroidJavaClass ("com.gustavosilva.myplugin.MainActivity");
 		AndroidJavaObject currentAct = unity.CallStatic<AndroidJavaObject> ("instance");
 		currentAct.Call("runOnUIThread",new AndroidJavaRunnable(()=> {
 			currentAct.Call ("shareText", "Titulo", "Corpo");}));
+			*/
+		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentAct = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		textField.text = currentAct.Call<string> ("DoIt");
+		currentAct.Call ("shareText", "Palmeiras", "Campeao");
 	}
 }
