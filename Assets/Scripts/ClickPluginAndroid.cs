32ec131b4136465885392926245cbc0f97f6e18a
Erik Balthazar <erik.balthazar@movile.com>
2015-07-16 11:57:23 -0300

    Add ClickPlugin script

---
+++ Assets/Scripts/ClickPluginAndroid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickPluginAndroid : MonoBehaviour {
+
+	void ClickUsePlugin() {
+		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentAct = unity.CallStatic<AndroidJavaObject> ("currentActivity");
+		currentAct.Call ("shareText", "Titulo", "Corpo");
+	}
+}
