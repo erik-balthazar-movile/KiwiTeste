824c81645816a731dc3240c8e87a5b99ee7e5007
Gustavo Bento <gustavo.silva@movile.com>
2015-07-16 14:19:08 -0300

    Some changes

--- Assets/Scripts/ClickPluginAndroid.cs
+++ Assets/Scripts/ClickPluginAndroid.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class ClickPluginAndroid : MonoBehaviour {
 
 	public void ClickUsePlugin() {
-		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentAct = unity.CallStatic<AndroidJavaObject> ("currentActivity");
-		currentAct.Call ("shareText", "Titulo", "Corpo");
+
+		AndroidJavaClass unity = new AndroidJavaClass ("com.gustavosilva.myplugin.MainActivity");
+		AndroidJavaObject currentAct = unity.CallStatic<AndroidJavaObject> ("instance");
+		currentAct.Call("runOnUIThread",new AndroidJavaRunnable(()=> {
+			currentAct.Call ("shareText", "Titulo", "Corpo");}));
 	}
 }
