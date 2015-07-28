65a7a71be7a43f4dca94a2a97e13b28092773ba7
Gustavo Bento <gustavo.silva@movile.com>
2015-07-28 10:46:40 -0300

    Improvement AndroidAndiOS scene

---
+++ Assets/Scripts/AndroidAndiOSPlugin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class AndroidAndiOSPlugin : MonoBehaviour {
+
+	public Text textField;
+	[DllImport ("__Internal")]
+	private static extern void openOurViewController();
+
+	public void OnClick(){
+#if UNITY_IOS
+		if(Application.platform == RuntimePlatform.IPhonePlayer){
+			openOurViewController();
+		}
+
+#elif UNITY_ANDROID
+		GameObject myText = new GameObject();
+		myText.AddComponent<CanvasRenderer> ();
+		myText.AddComponent<Text>();
+		myText.transform.parent = GameObject.Find("Canvas").transform;
+		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject currentAct = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		textField.text = currentAct.Call<string> ("DoIt");
+		currentAct.Call ("shareText", "Palmeiras", "Campeao");
+#endif
+	}
+}
---
+++ Assets/Scripts/ChangeText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+[RequireComponent (typeof (Text))]
+public class ChangeText : MonoBehaviour {
+
+	public string Androidtext;
+	public string iOStext;
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_ANDROID 
+		gameObject.GetComponent<Text>().text = Androidtext;
+
+#elif UNITY_IOS 
+		gameObject.GetComponent<Text>().text = iOStext;
+#endif
+	}
+
+}
